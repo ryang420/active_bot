00664649b8e5e2090a6104e2ae9b17901973e4a4
David Yang <David.Yang@activenetwork.com>
2019-01-29 16:01:27 +0800

    add duckling

--- run_nlu.py
+++ run_nlu.py
@@ -5,7 +5,7 @@ from rasa_nlu.model import Metadata, Interpreter
 
 def run_nlu():
     interpreter = Interpreter.load('./models/nlu/default/activenlu')
-    print(interpreter.parse("What's the weather like in London?"))
+    print(interpreter.parse("What's the weather like today in London?"))
 
 
 if __name__ == '__main__':
